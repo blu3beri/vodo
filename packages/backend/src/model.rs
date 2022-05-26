b0be5c840c187783002b67f81763da353f59e1c8
Moriarty <moritz@animo.id>
2022-05-26 15:55:20 +0200

    code review change
    Signed-off-by: Moriarty <moritz@animo.id>

--- packages/backend/src/model.rs
+++ packages/backend/src/model.rs
@@ -1,11 +1,11 @@
-extern crate mmap_storage;
+// extern crate mmap_storage;
 
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fs;
+// use std::fs;
 use uuid::Uuid;
 
-const STORAGE_PATH: &'static str = "notes.json";
+// const STORAGE_PATH: &'static str = "notes.json";
 
 /// State of a note
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -35,49 +35,49 @@ pub struct Notes {
 }
 
 impl Notes {
-    /// Load storage
-    pub fn load_storage(storage_path: Option<String>) {
-        match storage_path {
-            Some(path) => {
-                let mut storage =
-                    mmap_storage::file::Storage::open(path).expect("To create storage");
+    // /// Load storage
+    // pub fn load_storage(storage_path: Option<String>) {
+    //     match storage_path {
+    //         Some(path) => {
+    //             let mut storage =
+    //                 mmap_storage::file::Storage::open(path).expect("To create storage");
 
-                // TODO: how to fix below into and return the file contents
-                // let mut data: HashMap<String, Note> = storage.into();
-            }
-            None => {
-                let mut storage =
-                    mmap_storage::file::Storage::open(STORAGE_PATH).expect("To create storage");
-                // TODO: how to fix below into and return the file contents
-                // let mut data: HashMap<String, Note> = storage.into();
-            }
-        }
-    }
+    //             // TODO: how to fix below into and return the file contents
+    //             // let mut data: HashMap<String, Note> = storage.into();
+    //         }
+    //         None => {
+    //             let mut storage =
+    //                 mmap_storage::file::Storage::open(STORAGE_PATH).expect("To create storage");
+    //             // TODO: how to fix below into and return the file contents
+    //             // let mut data: HashMap<String, Note> = storage.into();
+    //         }
+    //     }
+    // }
 
-    /// Save the notes to file
-    pub fn save(&self, storage_path: Option<String>) {
-        match storage_path {
-            Some(path) => {
-                let mut storage =
-                    mmap_storage::file::Storage::open(path).expect("To create storage");
+    // /// Save the notes to file
+    // pub fn save(&self, storage_path: Option<String>) {
+    //     match storage_path {
+    //         Some(path) => {
+    //             let mut storage =
+    //                 mmap_storage::file::Storage::open(path).expect("To create storage");
 
-                // TODO: Figure out how to save to file with this method
-                // storage.put_data(self.map)
-            }
-            None => {
-                let mut storage =
-                    mmap_storage::file::Storage::open(STORAGE_PATH).expect("To create storage");
+    //             // TODO: Figure out how to save to file with this method
+    //             // storage.put_data(self.map)
+    //         }
+    //         None => {
+    //             let mut storage =
+    //                 mmap_storage::file::Storage::open(STORAGE_PATH).expect("To create storage");
 
-                // TODO: Figure out how to save to file with this method
-                // storage.put_data(self.map)
-            }
-        }
-    }
+    //             // TODO: Figure out how to save to file with this method
+    //             // storage.put_data(self.map)
+    //         }
+    //     }
+    // }
 
     /// Create an empty HashMap
     pub fn new() -> Self {
         // TODO: load from file here using above method when it works
-        let hash_map: HashMap<std::string::String, Note> = HashMap::new();
+        let hash_map: HashMap<String, Note> = HashMap::new();
         Self { map: hash_map }
     }
 
